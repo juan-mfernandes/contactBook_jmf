7d70a3976e1afd212b8f0b312dfb92c91e5df04c
juan-mfernandes <mfernandes.juan@gmail.com>
2024-05-17 13:57:20 -0300

    updating routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const server = express()
 const { contactRouter } = require('./routes/contacts')
+const { userRouter } = require('./routes/users')
 
 const port = 8080
 
@@ -14,3 +15,4 @@ server.use('/health', (req,res) => {
 
 server.use(express.json())
 server.use("/v1", contactRouter)
+server.use("/v1", userRouter)
